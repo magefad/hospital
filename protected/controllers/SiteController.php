bdc96db96f8d09d10d329350fcb84ae303750f8d
Ruslan Fadeev <fadeevr@gmail.com>
2014-02-12 00:25:34 +0400

    Tabs to spaces

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,24 +2,24 @@
 
 class SiteController extends CController
 {
-	/**
-	 * This is the default action that displays the phonebook Flex client.
-	 */
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
+    /**
+     * This is the default action that displays the phonebook Flex client.
+     */
+    public function actionIndex()
+    {
+        $this->render('index');
+    }
 
-	public function actionTest()
-	{
+    public function actionTest()
+    {
         echo '<pre>';
         Yii::setPathOfAlias('Sickness', Yii::getPathOfAlias('application.models.Sickness'));
 
-        $hospital = new Hospital();//первоначальные данные в конструкторе
+        $hospital = new Hospital(); //первоначальные данные в конструкторе
         $hospital->statistics(); // исходные данные
         $hospital->open(); //запись к врачам через терапевта
         $hospital->working(); //врачи лечат
 
         $hospital->statistics();
-	}
+    }
 }
